b42948349e8a18b8fde7f3899f7a4783b2c485f8
Alexander-wind <alexandrekamashindze@gmail.com>
2025-04-27 19:35:00 +0400

    GROUP 70

---
+++ GROUP 70/HW1.py
@@ -0,0 +1,17 @@
+print("Aleksandre Kamashidze") #saxeli
+print("mwvane") #ferebi
+print("cisperi")
+print(13) #megobrebis asaki
+print(13)
+print(14)
+print("Nini") #ojaxis wevrebis saxelebi
+print("Temo")
+print("Goirgi")
+print("Elene")
+myavs_shinauri_cxoveli = True #kitxva (bool)
+myavs_shinauri_prinveli = False
+
+#string-simbolo
+#integer-ricxvi
+#bool-michneva True an False it rame informacias
+#float-nomeruli informacia
