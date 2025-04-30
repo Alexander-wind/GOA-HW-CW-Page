42a4946732c6daf0a8db66e2e7bb283132753153
Alexander-wind <alexandrekamashindze@gmail.com>
2025-04-30 21:35:10 +0400

    Level 0

---
+++ Level 1/CW1.py
@@ -0,0 +1,12 @@
+data type - მონაცემთა ტიპი
+
+string-str
+integer-int
+float-float
+boolean-bool
+
+
+str-text info- "Hello world"
+int-number info- 5, 10, 15, 20, 25, 30
+float-number info- 5.0, 10.0, 15.0, 20.0, 25.0, 30.0
+bool-True, False
---
+++ Level 2/CW2.py
@@ -0,0 +1,13 @@
+"yuti"
+19
+dres_kviraa = True 
+amindi = 19.5
+
+
+count = 0
+
+
+count = count + 1
+
+#name = input("What is your name?")
+ 
