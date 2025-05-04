c461d5d3723025e06a4e84a3c08d603d7589cfc1
Alexander-wind <alexandrekamashindze@gmail.com>
2025-05-04 21:09:55 +0400

    Level 4

---
+++ Level 4/CW4.py
@@ -0,0 +1,29 @@
+#CW chanaweri
+
+# 1) no spaces
+# 2) can't start with a number
+# 3) A-Z, a-z, 0-9
+
+"""name = "alex"
+print("hello " + name)
+
+name1 =input()
+print("hello " + name1)
+
+#str + str = concatenation
+
+Name = "Alex"
+surname = "Kamashidze"
+age = "20"
+hobby = "programming"
+print(f"My name is {Name} and my surname is {surname}.  I am {age} years old. My hobby is {hobby}.")
+"""
+
+#CW damoukidebeli
+
+Name1 =input("enter your name:")
+Surname1 =input("enter your surname:")
+Age1 =input("enter your age:")
+Hobby1 =input("eter your hobby:")
+Friends_name1 =input("enter your friends name:")
+print(f"My name is {Name1} and my surname is {Surname1}. I am {Age1} years old. My hobby is {Hobby1}. My friends name is {Friends_name1}.")
