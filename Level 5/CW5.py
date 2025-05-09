521f17bbed5ad4cad1a1bfe0af0c51a0158d56f9
Alexander-wind <alexandrekamashindze@gmail.com>
2025-05-09 22:57:13 +0400

    Level 5

---
+++ Level 5/CW5.py
@@ -0,0 +1,48 @@
+# +,-,*,/,%,<,>,==,<=,>=,!=,**
+""""hello world" #string
+10-5 #integer
+"home" * 3 #string
+67 % 10 #integer
+print(3 > 4) #boolean
+name = "Alex" #boolean
+print(3 != 4) #boolean
+4 ** 2 #integer
+"""
+"""print("hello world"[0]) #string"""
+
+
+
+#CW
+"""x=input()
+print(x[0])"""
+
+'''y=input()
+z=input()
+print(y+z)'''
+
+'''x=input()
+if x == "yes":
+    print("What is your name?")
+y=input()
+print("bye " + y)          '''
+
+'''w=input()
+if w == "A":
+    print(100)
+if w == "B":
+    print(80)
+if w == "C":
+    print(60)
+if w == "D":
+    print(40)
+if w == "F":
+    print("Failed!")
+'''
+
+"""a=input()
+b=input()
+if a == b:
+    print("True")
+if a != b:
+    print("False")"""
+
