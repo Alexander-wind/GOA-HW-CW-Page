e2518166a9d0d356bf89062f3ece281b854079ca
Alexander-wind <alexandrekamashindze@gmail.com>
2025-05-11 20:50:39 +0400

    Level 5

---
+++ Level 5/HW5.py
@@ -0,0 +1,11 @@
+x=int(input())
+if x >= 10:
+    print("you are right!")
+
+y=int(input())
+z=int(input())
+
+print(y+z, y-z, y*z, y/z, y%z, y**z, y<z, y>z, y==z, y!=z, y<=z, y>=z)
+
+#if ფუნცქია "თუ" განსაზღვრავს კოდის მიმართულებას, რომელიც კონკრეტულ პირობებზეა დამოკიდებული მაგ: x=12 y=5 if x+y=17:
+#     print("the answer's 17")
