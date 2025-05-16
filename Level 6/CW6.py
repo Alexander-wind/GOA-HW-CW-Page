336a76d04f106b5408ff1ea8e19d876ef1bb54e3
Alexander-wind <alexandrekamashindze@gmail.com>
2025-05-16 22:54:43 +0400

    Level 6

---
+++ Level 6/CW6.py
@@ -0,0 +1,17 @@
+"""while  3<10:                      #1
+    print("Endelss loop")"""
+
+"""b=0                                #2
+while b<10:
+    print("loop")
+    b+1"""
+
+"""while 4<10: #თავი                 #3
+    print("lalolo") #ტანი
+"""
+
+"""x = int(input("Enter a number: "))       #4
+while x != 100:
+    print("Try again")
+    x = int(input("Enter a number: "))
+"""
