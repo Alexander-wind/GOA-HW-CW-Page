9a0a5fd97de27e258b1ef42e08fc0edd495aca45
Alexander-wind <alexandrekamashindze@gmail.com>
2025-05-17 21:42:48 +0400

    Level 7

---
+++ Level 7/HW7.py
@@ -0,0 +1,27 @@
+x=1                #1
+while x<=10:
+    print(x)
+    x=x+1
+
+y=10                #2
+while y>=1:
+    print(y)
+    y=y-1
+
+#while loop-პროცესი რომელიც მიმდინარეობს მანამ სანამ მოცემული პირობა შეიცვლება, თუ არა და ის სამუდამოდ გაგრძელდება   #3
+
+z = 'python123'                                 #4 აქ, როდესაც თავიდან მომხმარებელი წერს პაროლს, და ის არ შეესაბამება,   
+a = input('Input a password: ')                 # იმის შემდეგ რაც Password is incorrect დაიპრინტება,
+                                                # პროგრამა მას თავიდან სტხოვს მას თვით while loopში და თავიდან აწერინებს.  
+while a != z:                                   # ეს გაგრძელდება სანამ ის არ შეიყვანს სწორ პაროლს
+    print('Password is incorrect')
+    a = input('Try again: ')
+
+print('Password is correct')
+
+b=input('Enter a number: ')  #5
+c=1
+b=int(b)         
+while c<=b:
+    print(c)
+    c=c+1      
