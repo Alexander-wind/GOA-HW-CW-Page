88fbe9376d1aff60de8b6a36166bb9fa8e6275c2
Alexander-wind <alexandrekamashindze@gmail.com>
2025-05-23 20:31:09 +0400

    Level 7

---
+++ Level 7/HW7.py
@@ -0,0 +1,15 @@
+x=1
+for i in range(0, 10, 1):
+    print(x)
+    x+=1
+
+y='Hello world'
+for i in range(0,5,1):
+    print(y)
+
+z=0
+for i in range(-1, 20, 2):
+    print(z)
+    z+=2
+
+#for loop-ოპერატორი, რომელიც გვაძლევს გავიმეოროთ გარკვეული ოპერაცია რამდენჯერმე
