6776c58a7b1923c77a26d1f24b3b1656c1f0bf67
Alexander-wind <alexandrekamashindze@gmail.com>
2025-06-27 19:54:06 +0400

    GOA_CW-HW

---
+++ Level 17/Classwork_17/CW1.py
@@ -0,0 +1,6 @@
+#შექმენით List, List-ში შეინახეთ 4 მანქანა და შემდეგ მე-1 და მე-2 ინდექსი შეცვალეთ, საბოლოოდ დაპრიტეთ მთლიანი List-ი
+
+cars = ["Toyota", "Honda", "Ford", "Chevrolet"]
+cars[0] = "Nissan"  
+cars[1] = "Maklaren"
+print(cars) 
