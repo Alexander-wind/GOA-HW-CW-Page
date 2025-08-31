6675811a2ef0fd3539d9f7443fad57c1c8b2b6a4
Alexander-wind <alexandrekamashindze@gmail.com>
2025-08-31 19:15:47 +0400

    GOA_CW-HW

---
+++ Level 25/Homework_25/HW3.py
@@ -0,0 +1,14 @@
+# შექმენით def ფუნქცია სახელად greet - მას უნდა ჰქონდეს 1 არგუმენტი name და  უნდა დაწერე მისალმების ფუნქცია 
+
+def greet(name):
+    print(f"Hello {name}")
+
+greet("Alex")
+
+#ან
+
+def greet():
+    name = input("Enter your name: ")
+    print(f"Hello {name}")
+
+greet()
